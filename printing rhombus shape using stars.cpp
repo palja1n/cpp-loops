77b8eebc5f76994f9a10e468ab83d98375a1f5c5
Pal Jain <paljaiin@gmail.com>
2025-07-28 00:42:07 +0530

    Create printing rhombus shape using stars.cpp

---
+++ printing rhombus shape using stars.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j, k;
+    int n=5;
+    for (i=1; i<=n; i++)
+        {
+        for (k=1; k<=n-i; k++)
+        cout<<"  ";
+    {    
+
+        for(j=1; j<=n; j++)
+        cout<<"* ";
+        cout<<endl;
+    }
+        }
+
+    return 0;
+}
+
+/*
+Output:
+        * * * * * 
+      * * * * * 
+    * * * * * 
+  * * * * * 
+* * * * * 
+  */
